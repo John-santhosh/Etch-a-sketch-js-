7aabc9082f3ceb21a13eb0c468904eef16cf5316
john <johnsantosh2@gmail.com>
2023-03-23 10:46:11 +0530

    UI updated

--- script/index.js
+++ script/index.js
@@ -1,98 +1,97 @@
 // let gridBtn = document.querySelector("#grids");
 let parentGrid = document.querySelector(".parent");
-let range = document.querySelector('#range')
+let range = document.querySelector("#range");
 // let limit = document.querySelector('#limit')
 let colorPlate = document.querySelector("#colorPalate");
-let clearBtn = document.querySelector('#clearBtn')
+let clearBtn = document.querySelector("#clearBtn");
 let displayRange = document.querySelector("#range-val");
-let rainBow = document.querySelector('.rainbow')
+let rainBow = document.querySelector(".rainbow");
 let getClr = document.querySelectorAll(".getclr");
-let palateColor= document.querySelector('.palateColor')
+let palateColor = document.querySelector(".palateColor");
 
-// Changing user preferred colors 
-let color = generateRainbow;
+// Changing user preferred colors
 
-// clearing previous grids when range is clicked 
-range.addEventListener('click',()=> clearChildren())
+// clearing previous grids when range is clicked
+range.addEventListener("click", () => clearChildren());
 
-// creating new grids when range is clicked 
-range.addEventListener('click',functionMain)
+// creating new grids when range is clicked
+range.addEventListener("click", functionMain);
 
 // creating colors
-range.addEventListener('click', createColors)
-function createColors(){
-        // calling mouse trail method 
-        console.log(color)
-        let hovering = document.querySelectorAll('.check')
-        hovering.forEach(child=>{
-        child.addEventListener("mouseenter",color)
-    })
+range.addEventListener("click", createColors);
+let color = generateRainbow;
+
+function createColors() {
+  // calling mouse trail method
+  console.log(color);
+  let hovering = document.querySelectorAll(".check");
+  hovering.forEach((child) => {
+    child.addEventListener("mouseover", color);
+  });
 }
 
-function functionMain(){
-    // displaying the range value dynamically 
-    displayRange.textContent = `${range.value} * ${range.value}`;
-    
-    // getting the limit of the grids 
-    let grid = range.value; ;
+function functionMain() {
+  // displaying the range value dynamically
+  displayRange.textContent = `${range.value} * ${range.value}`;
 
-    // considering resources and memory limiting the total grids upto 30*30
-    if(grid>30) return alert("maximum limit : 30")
+  // getting the limit of the grids
+  let grid = range.value;
 
-    // creating grid children 
-    for(let i=0 ; i< grid*grid ; i++){
-    let div = document.createElement('div')
+  // considering resources and memory limiting the total grids upto 30*30
+  if (grid > 30) return alert("maximum limit : 30");
+
+  // creating grid children
+  for (let i = 0; i < grid * grid; i++) {
+    let div = document.createElement("div");
     div.classList.add("check");
     parentGrid.style.gridTemplateColumns = `repeat(${grid} , 1fr)`;
     parentGrid.style.gridTemplateRows = `repeat(${grid} , 1fr)`;
-    parentGrid.appendChild(div)
-    }
+    parentGrid.appendChild(div);
+  }
 }
 
-colorPlate.addEventListener('click',function(){
-    color= mouseTrail;
-    // range.value = 0; 
-    createColors();
-    // console.log("mouse")
-})
+colorPlate.addEventListener("click", function () {
+  color = mouseTrail;
+  // range.value = 0;
+  createColors();
+  // console.log("mouse")
+});
 
-rainBow.addEventListener('click',function(){
-    color= generateRainbow;
-    // range.value = 0;
-    createColors();
-    // console.log('rainbow')
-})
-
+rainBow.addEventListener("click", function () {
+  color = generateRainbow;
+  // range.value = 0;
+  createColors();
+  // console.log('rainbow')
+});
 
 //adding mouse trail effect / setting color
-function mouseTrail(){
-    // Here <this> refer the <child> from forEach 
-    this.style.background=colorPlate.value
-    // return colorPlate.value;
+function mouseTrail() {
+  // Here <this> refer the <child> from forEach
+  this.style.background = colorPlate.value;
+  // return colorPlate.value;
 }
 
 // generating rainbow colors
-function generateRainbow(){
-    this.style.background = `rgb(${rand()},${rand()},${rand()})`;
-//   return `rgb(${rand()},${rand()},${rand()})`;
+function generateRainbow() {
+  this.style.background = `rgb(${rand()},${rand()},${rand()})`;
+  //   return `rgb(${rand()},${rand()},${rand()})`;
 }
 
-//Generating  random number 
-function rand(){
-    return Math.floor(Math.random() * 255);
+//Generating  random number
+function rand() {
+  return Math.floor(Math.random() * 255);
 }
 
 // clearing out the container
-function clearChildren(){
-    let child = parentGrid.children;
-    Array.from(child).forEach(a=>parentGrid.removeChild(a));
+function clearChildren() {
+  let child = parentGrid.children;
+  Array.from(child).forEach((a) => parentGrid.removeChild(a));
 }
 
-clearBtn.addEventListener('click',clearChildren)
-
+clearBtn.addEventListener("click", clearChildren);
 
 // setting the slider value to 0 when clear btn is pressed
-clearBtn.addEventListener('click',function(){
-    range.value = 0;
-    displayRange.textContent = range.value;
-})
+clearBtn.addEventListener("click", function () {
+  range.value = 0;
+  displayRange.textContent = range.value;
+});
